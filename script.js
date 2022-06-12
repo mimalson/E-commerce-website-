e84f46d6afab1516bd781c07e4269ccefc76380d
Amina <87755052+mimalson@users.noreply.github.com>
2022-06-12 21:14:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const sideNavbar = document.getElementById('side-navbar');
 navShowBtn.addEventListener('click', () => {
     sideNavbar.classList.add('side-navbar-show');
 });
+
+navHideBtn.addEventListener('click', () => {
+    sideNavbar.classList.remove('side-navbar-show');
+});
+
+// category
+const categoryItems = document.getElementById('category-list-items');
+const categoryTogglerBtn = document.querySelector('.category-toggler-btn');
+categoryTogglerBtn.addEventListener('click', () => {
+    categoryItems.classList.toggle('show-category-items');
+
+    if(categoryItems.classList.contains('show-category-items')){
+        categoryTogglerBtn.querySelector('i').className = "fas fa-circle-arrow-up";
+    } else{
+        categoryTogglerBtn.querySelector('i').className = "fas fa-circle-arrow-down";
+    }
+})
