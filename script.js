19dea167be5866100d9a63a30607af8039950882
Amina <87755052+mimalson@users.noreply.github.com>
2022-06-12 21:13:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// side navbar
+const navShowBtn = document.querySelector('.navbar-show-btn');
+const navHideBtn = document.querySelector('.navbar-hide-btn');
+const sideNavbar = document.getElementById('side-navbar');
+navShowBtn.addEventListener('click', () => {
+    sideNavbar.classList.add('side-navbar-show');
+});
